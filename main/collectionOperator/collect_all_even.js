96b85481e7cb04849ab9541785c017c4fdaefdc9
sidashen <shashasi8023@163.com>
2019-12-30 17:53:11 +0800

    [shasha] feat: collect-all-even

--- main/collectionOperator/collect_all_even.js
+++ main/collectionOperator/collect_all_even.js
@@ -2,6 +2,13 @@
 
 function collect_all_even(collection) {
   //在这里写入代码
+  var newArr = [];
+    collection.forEach(item => {
+      if (item % 2 === 0) { 
+        newArr.push(item);
+      }
+    });
+  return newArr;
 }
 
 module.exports = collect_all_even;
