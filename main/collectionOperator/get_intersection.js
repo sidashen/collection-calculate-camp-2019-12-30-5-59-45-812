0d4f9d0ab3e35c06544e23938b73dbb2445096b0
sidashen <shashasi8023@163.com>
2019-12-31 10:51:09 +0800

    [shasha] feat: get_intersection

--- main/collectionOperator/get_intersection.js
+++ main/collectionOperator/get_intersection.js
@@ -2,6 +2,17 @@
 
 function get_intersection(collection_a, collection_b) {
   //在这里写入代码
+  var newArr = [];
+  var length_a = collection_a.length;
+  var length_b = collection_b.length;
+  for (var i = 0; i < length_b; i++) {
+    for (var j = 0; j < length_a; j++) {
+      if (collection_a[j] === collection_b[i]) {
+        newArr.push(collection_a[j]);
+      }
+    }
+  }
+  return newArr;
 }
 
 module.exports = get_intersection;
