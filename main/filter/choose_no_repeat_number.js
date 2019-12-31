34cf2f227a2b01e13fa90f0d9cdfd91b1bacb1aa
sidashen <shashasi8023@163.com>
2019-12-31 17:17:02 +0800

    [shasha] feat: choose_norepeat_number

--- main/filter/choose_no_repeat_number.js
+++ main/filter/choose_no_repeat_number.js
@@ -1,8 +1,11 @@
 'use strict';
 
 function choose_no_repeat_number(collection) {
-
   //在这里写入代码
+  var collection = [1, 1, 1, 2, 2, 3, 4];
+  var newString = new Set(collection);
+  var newArr = Array.from(newString);
+  return newArr;
 }
 
 module.exports = choose_no_repeat_number;
