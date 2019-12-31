3ed73a9a3fe883a1082e8e7c6302c6aa74287d93
sidashen <shashasi8023@163.com>
2019-12-31 17:09:32 +0800

    [shasha] feat: choose_multiples_of_three

--- main/filter/choose_multiples_of_three.js
+++ main/filter/choose_multiples_of_three.js
@@ -1,8 +1,10 @@
 'use strict';
 
 function choose_multiples_of_three(collection) {
-
   //在这里写入代码
+  return collection.filter((item) => {
+    return item % 3 === 0;
+  })
 }
 
 module.exports = choose_multiples_of_three;
