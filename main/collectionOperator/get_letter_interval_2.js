984ea75453c1e3fc3e674e8eaa12f7eaf5172a64
sidashen <shashasi8023@163.com>
2019-12-31 16:38:10 +0800

    [shasha] refactor: get_letter_interval_2

--- main/collectionOperator/get_letter_interval_2.js
+++ main/collectionOperator/get_letter_interval_2.js
@@ -4,41 +4,44 @@ function get_letter_interval_2(number_a, number_b) {
   //在这里写入代码
   var newArr = [];
   var item;
+  var itemTens;
+  var itemOnes;
   if (number_a < number_b) {
     for (var i = number_a; i <= 26; i++) {
       item = String.fromCharCode(96 + i);
       newArr.push(item);
     }
     for (var i = 27; i <= number_b; i++) {
-      var itemTens = Math.floor((i - 1) / 26);
-      var itemOnes = i - itemTens * 26;
-      item = `${String.fromCharCode(96 + itemTens)}${String.fromCharCode(96 + itemOnes)}`;
-      newArr.push(item);
+      ({ itemTens, itemOnes, item } = doubleWords(i, item, newArr));
     }
   } else if (number_a > number_b) {
-    for (i = number_a; i >= 27; i--) {
-      var itemTens = Math.floor((i - 1) / 26);
-      var itemOnes = i - itemTens * 26;
-      item = `${String.fromCharCode(96 + itemTens)}${String.fromCharCode(96 + itemOnes)}`;
-      newArr.push(item);
+    for (var i = number_a; i >= 27; i--) {
+      ({ itemTens, itemOnes, item } = doubleWords(i, item, newArr));
     }
     for (var i = 26; i >= number_b; i--) {
       item = String.fromCharCode(96 + i);
       newArr.push(item);
     }
   } else if (number_a = number_b) {
-      if (number_a <= 26) {
-        item = String.fromCharCode(96 + number_a);
+      var i = number_a;
+      if (i <= 26) {
+        item = String.fromCharCode(96 + i);
         newArr.push(item);
-      } else if (number_a > 26) {
-        var itemTens = Math.floor((number_a - 1) / 26);
-        var itemOnes = number_a - itemTens * 26;
-        item = `${String.fromCharCode(96 + itemTens)}${String.fromCharCode(96 + itemOnes)}`;
-        newArr.push(item);
+      } else if (i > 26) {
+        ({ itemTens, itemOnes, item } = doubleWords(i, item, newArr));
       }
   }
   return newArr;
 }
 
+function doubleWords(i, item, newArr) {
+  var itemTens = Math.floor((i - 1) / 26);
+  var itemOnes = i - itemTens * 26;
+  item = `${String.fromCharCode(96 + itemTens)}${String.fromCharCode(96 + itemOnes)}`;
+  newArr.push(item);
+  return { itemTens, itemOnes, item };
+}
+
 module.exports = get_letter_interval_2;
+
 
