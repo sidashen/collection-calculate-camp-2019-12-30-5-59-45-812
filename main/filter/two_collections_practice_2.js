1f41f3acdb8fedccb5697b8eb150d1a8a389b007
sidashen <shashasi8023@163.com>
2019-12-31 18:12:19 +0800

    [shasha] feat: two_collections_practice_2

--- main/filter/two_collections_practice_2.js
+++ main/filter/two_collections_practice_2.js
@@ -1,8 +1,11 @@
 'use strict';
 
 function choose_no_common_elements(collection_a, collection_b) {
-
   //在这里写入代码
+  var newArr = collection_a.filter(function (item) {
+    return collection_b.indexOf(item) == -1;
+  })
+  return newArr;
 }
 
 module.exports = choose_no_common_elements;
