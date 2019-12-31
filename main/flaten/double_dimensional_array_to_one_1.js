60d450f388cae1f88269f3427df2cd089fc33b5e
sidashen <shashasi8023@163.com>
2019-12-31 18:47:29 +0800

    [shasha] feat: double_dimensional_array_to_one_1

--- main/flaten/double_dimensional_array_to_one_1.js
+++ main/flaten/double_dimensional_array_to_one_1.js
@@ -1,8 +1,18 @@
 'use strict';
 
 function double_to_one(collection) {
-
   //在这里写入代码
+  var newArr = [];
+  for (var i = 0; i < collection.length; i++) {
+    if (collection[i] instanceof Array) {
+      for (var j = 0; j < collection[i].length; j++) {
+        newArr.push(collection[i][j]);
+      }
+    } else {
+        newArr.push(collection[i]);
+    }
+  }
+  return newArr;
 }
 
 module.exports = double_to_one;
