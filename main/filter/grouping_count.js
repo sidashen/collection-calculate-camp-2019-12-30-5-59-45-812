14e5b7340774ecc3e71aae4a882ea6aa4bf4245c
sidashen <shashasi8023@163.com>
2019-12-31 17:47:30 +0800

    [shasha] feat: group_count

--- main/filter/grouping_count.js
+++ main/filter/grouping_count.js
@@ -1,8 +1,13 @@
 'use strict';
 
 function grouping_count(collection) {
-
   //在这里写入代码
+  var newObj = {};
+  var collection = [1,1,1,1,2,3,1,3,4,2,3,1,3,4,2];
+	collection.forEach(function(e){
+    newObj[e] = newObj[e] >= 1 ? newObj[e] + 1 : 1;
+  });
+  return newObj;
 }
 
 module.exports = grouping_count;
