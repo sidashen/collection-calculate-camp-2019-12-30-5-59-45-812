35f7032b0902f75be07cecffe92a93ff9a71551d
sidashen <shashasi8023@163.com>
2019-12-31 18:31:22 +0800

    [shasha] feat: two_collections_practice_3

--- main/filter/two_collections_practice_3.js
+++ main/filter/two_collections_practice_3.js
@@ -1,8 +1,16 @@
 'use strict';
 
 function choose_divisible_integer(collection_a, collection_b) {
-
   //在这里写入代码
+  var newArr = [];
+  for (var i = 0; i < collection_a.length; i++) {
+    for (var j = 0; j < collection_b.length; j++) {
+      if (collection_a[i] % collection_b[j] == 0) {
+        newArr.push(collection_a[i]);
+      }
+    }
+  }
+  return newArr;
 }
 
 module.exports = choose_divisible_integer;
