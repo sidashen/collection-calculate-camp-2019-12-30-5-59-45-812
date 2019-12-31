eee7468660c24ccf024357c4159774e984629ff2
sidashen <shashasi8023@163.com>
2019-12-31 10:38:34 +0800

    [shasha] feat: get_integer_interval

--- main/collectionOperator/get_integer_interval.js
+++ main/collectionOperator/get_integer_interval.js
@@ -2,6 +2,19 @@
 
 function get_integer_interval(number_a, number_b) {
   //在这里写入代码
+  var newArr = [];
+  if (number_a < number_b) {
+    for (var i = number_a; i <= number_b; i++) {
+        newArr.push(i);
+    }
+  } else if (number_a > number_b) {
+    for (var i = number_a; i >= number_b; i--) {
+        newArr.push(i);
+    }
+  } else if (number_a = number_b) {
+    newArr.push(number_a);
+  }
+  return newArr;
 }
 
 module.exports = get_integer_interval;
