1dccb0c77b1f59fc140020c527ec59fd7bbe87c3
sidashen <shashasi8023@163.com>
2020-01-01 10:34:29 +0800

    [shasha] feat: rank_desc

--- main/map/rank_desc.js
+++ main/map/rank_desc.js
@@ -1,6 +1,9 @@
 'use strict';
 var rank_desc = function(collection){
-  
+  collection.sort(function (value1, value2) {
+    return value1 - value2;
+  })
+  return collection;
 };
 
 module.exports = rank_desc;
