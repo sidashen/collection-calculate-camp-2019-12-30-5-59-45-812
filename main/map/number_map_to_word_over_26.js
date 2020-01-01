55402bf5bbf280a1d60eba45d799cc75ab37492d
sidashen <shashasi8023@163.com>
2020-01-01 10:14:50 +0800

    [shasha] feat: number_map_to_word_over_26

--- main/map/number_map_to_word_over_26.js
+++ main/map/number_map_to_word_over_26.js
@@ -1,6 +1,16 @@
 'use strict';
-var number_map_to_word_over_26 = function(collection){
-  return ['a','m','aa','ad','y','aa'];
-};
+function number_map_to_word_over_26(collection) {
+  var newArr = collection.map(item => {
+    if (item <= 26) {
+      item = String.fromCharCode(96 + item);
+    } else if (item >= 27) {
+      var itemFormer = Math.floor((item - 1) / 26);
+      var itemLatter = item - itemFormer * 26;
+      item = `${String.fromCharCode(96 + itemFormer)}${String.fromCharCode(96 + itemLatter)}`;
+    }
+    return item;
+  });
+  return newArr;
+}
 
 module.exports = number_map_to_word_over_26;
