f66ef6af30c47269f287d1fd4a412df6dc96097a
sidashen <shashasi8023@163.com>
2020-01-01 14:47:05 +0800

    [shasha] feat: section_1_practice_2

--- main/section_1/practice_2/practice.js
+++ main/section_1/practice_2/practice.js
@@ -1,5 +1,15 @@
 function collect_same_elements(collection_a, collection_b) {
   //在这里写入代码
+  var newCollection_b = [];
+  for (var i = 0; i < collection_b.length; i++) {
+      for (var j = 0; j < collection_b[i].length; j++) {
+        newCollection_b.push(collection_b[i][j]);
+      }
+    } 
+  var newArr = collection_a.filter(function (item) {
+    return newCollection_b.indexOf(item) !== -1;
+  });
+  return newArr;
 }
 
 module.exports = collect_same_elements;
