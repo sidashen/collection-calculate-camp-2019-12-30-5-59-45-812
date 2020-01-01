330e0e91e1c5ec7c8fcc9795a2273736b1f9e003
sidashen <shashasi8023@163.com>
2020-01-01 11:01:35 +0800

    [shasha] feat: compute_average

--- main/reduce/compute_average.js
+++ main/reduce/compute_average.js
@@ -2,6 +2,12 @@
 
 function compute_average(collection) {
   //在这里写入代码
+  var totalNum = 0;
+  var count = collection.length;
+  for (var i = 0; i < count; i++) {
+    totalNum += collection[i];
+  }
+  return totalNum / count;
 }
 
 module.exports = compute_average;
