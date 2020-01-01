0610d25ff267762e52fa5c5202fd16f9d4806c96
sidashen <shashasi8023@163.com>
2020-01-01 13:25:53 +0800

    [shasha] feat: find_element_first_subscript

--- main/reduce/find_element_last_subscript.js
+++ main/reduce/find_element_last_subscript.js
@@ -2,6 +2,13 @@
 
 function calculate_elements_sum(collection, element) {
   //在这里写入代码
+  var index;
+  for (var i = 0; i < collection.length; i++) {
+    if (element == collection[i]) {
+      index = i;
+    }
+  }
+  return index;
 }
 
 module.exports = calculate_elements_sum;
