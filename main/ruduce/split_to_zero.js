596053945231cd53d278624c49d32501d933739a
sidashen <shashasi8023@163.com>
2020-01-01 14:10:35 +0800

    [shasha] feat: split_to_zero

--- main/ruduce/split_to_zero.js
+++ main/ruduce/split_to_zero.js
@@ -2,6 +2,21 @@
 
 function spilt_to_zero(number, interval) {
   //在这里写入代码
+  var newArr = [number];
+  if (interval == 0.3) {
+    while (number > -0.2) {
+      number = Math.round((number - interval) * 10) / 10; //保留一位小数
+      newArr.push(number);
+    }
+  } else {
+    while (number > 0) {
+      number = Math.round((number - interval) * 10) / 10; //保留一位小数
+      newArr.push(number);
+    }
+  }
+  return newArr;
 }
 
 module.exports = spilt_to_zero;
+
+
