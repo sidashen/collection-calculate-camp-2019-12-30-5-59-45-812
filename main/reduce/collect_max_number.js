6dff9ef9475a3d2d10c5ca575da6a49f9684fa7f
sidashen <shashasi8023@163.com>
2020-01-01 10:47:26 +0800

    [shasha] feat: collect max & min number

--- main/reduce/collect_max_number.js
+++ main/reduce/collect_max_number.js
@@ -2,6 +2,21 @@
 
 function collect_max_number(collection) {
   //在这里写入代码
+  var maxNum = Math.max.apply(null, collection);
+  return maxNum;
+
 }
+
+//另一种方法
+// function collect_max_number(collection) {
+//   //在这里写入代码
+//   var maxNum = 0;
+//   collection.forEach(item => {
+//     if (item >= maxNum) {
+//       maxNum = item;
+//     }
+//   });
+//   return maxNum;
+// }
 
 module.exports = collect_max_number;
--- main/reduce/collect_min_number.js
+++ main/reduce/collect_min_number.js
@@ -2,6 +2,8 @@
 
 function collect_min_number(collection) {
   //在这里写入代码
+  var minNum = Math.min.apply(null, collection);
+  return minNum;
 }
 
 module.exports = collect_min_number;
