defefa5cf2d0eb3abb13cd9c1b9f25ec6bb3800c
sidashen <shashasi8023@163.com>
2020-01-01 13:31:59 +0800

    [shasha] feat: find_last_even

--- main/reduce/find_last_even.js
+++ main/reduce/find_last_even.js
@@ -2,6 +2,13 @@
 
 function find_last_even(collection) {
   //在这里写入代码
+  var lastEvenNum;
+  for (var i = 0; i < collection.length; i++) {
+    if (collection[i] % 2 == 0) {
+      lastEvenNum = collection[i];
+    }
+  }
+  return lastEvenNum;
 }
 
 module.exports = find_last_even;
