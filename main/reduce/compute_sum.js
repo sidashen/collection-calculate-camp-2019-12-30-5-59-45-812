2565f649511179036e72158b1259b562cddfda45
sidashen <shashasi8023@163.com>
2020-01-01 13:19:27 +0800

    [shasha] feat: compute_sum

--- main/reduce/compute_sum.js
+++ main/reduce/compute_sum.js
@@ -2,6 +2,11 @@
 
 function calculate_elements_sum(collection) {
   //在这里写入代码
+  var sum = 0;
+  collection.forEach(item => {
+    sum += item;
+  })
+  return sum;
 }
 
 module.exports = calculate_elements_sum;
