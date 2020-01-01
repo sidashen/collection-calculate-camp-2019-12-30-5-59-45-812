2f67e7366fb68baeead34775422aa67b5b8dbd22
sidashen <shashasi8023@163.com>
2020-01-01 13:29:31 +0800

    [shasha] feat: find_element_last_subscript

--- main/reduce/find_first_even.js
+++ main/reduce/find_first_even.js
@@ -2,6 +2,14 @@
 
 function find_first_even(collection) {
   //在这里写入代码
+  var firstEvenNum;
+  for (var i = 0; i < collection.length; i++) {
+    if (collection[i] % 2 == 0) {
+      firstEvenNum = collection[i];
+      break;
+    }
+  }
+  return firstEvenNum;
 }
 
 module.exports = find_first_even;
