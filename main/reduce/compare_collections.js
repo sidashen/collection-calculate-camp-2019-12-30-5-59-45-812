43ef2865271b3714b0e768f643e273b52783ccf6
sidashen <shashasi8023@163.com>
2020-01-01 10:53:13 +0800

    [shasha] feat: compare_collections

--- main/reduce/compare_collections.js
+++ main/reduce/compare_collections.js
@@ -2,6 +2,19 @@
 
 function compare_collections(collection_a, collection_b) {
   //在这里写入代码
+  var flag = true;
+  if (collection_a.length == collection_b.length) {
+    for (var i = 0; i < collection_a.length; i++) {
+      if (collection_a[i] == collection_b[i]) {
+        flag = true;
+      } else {
+        flag = false;
+      }
+    }
+  } else {
+    flag = false;
+  }
+  return flag;
 }
 
 module.exports = compare_collections;
