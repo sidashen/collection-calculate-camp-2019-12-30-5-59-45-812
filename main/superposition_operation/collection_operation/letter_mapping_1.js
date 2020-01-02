129be23c1784048ed5289d06cc3b5c8e6d30b91c
sidashen <shashasi8023@163.com>
2020-01-02 23:36:40 +0800

    [shasha] feat: letter_mapping_1

--- main/superposition_operation/collection_operation/letter_mapping_1.js
+++ main/superposition_operation/collection_operation/letter_mapping_1.js
@@ -1,8 +1,15 @@
 'use strict';
 
 function even_to_letter(collection) {
-
   //在这里写入代码
+  let newArr = [];
+
+  collection.forEach(item => {
+    if (item % 2 == 0) {
+      newArr.push(String.fromCharCode(96 + item));
+    }
+  });
+  return newArr;
 }
 
 module.exports = even_to_letter;
