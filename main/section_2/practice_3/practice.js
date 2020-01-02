497de2f2e15c2fe62c65f63bb80e617261602a30
sidashen <shashasi8023@163.com>
2020-01-02 23:04:21 +0800

    [shasha] feat: section_2_practice_3

--- main/section_2/practice_3/practice.js
+++ main/section_2/practice_3/practice.js
@@ -1,5 +1,27 @@
 function count_same_elements(collection) {
   //在这里写入代码
+  let result = [];
+
+  for (let item of collection) {
+    var key = item.match(/\d/) ? item.slice(0, 1) : item;
+    var num = item.match(/\d/) ? parseInt(item.match(/\d/g).join("")) : 1;
+    let obj = finds(result, key);
+      if (obj) {
+        obj.summary += num;
+      } else {
+        result.push({name: key, summary: num});
+        }
+  }
+  return result;
+}
+
+function finds(result, element) {
+  for (let item of result) {
+    if (item.name === element) {
+      return item;
+    }
+  }
+  return null;
 }
 
 module.exports = count_same_elements;
--- main/section_3/practice_1/practice.js
+++ main/section_3/practice_1/practice.js
@@ -1,5 +1,21 @@
 function create_updated_collection(collection_a, object_b) {
   //在这里写入代码
+  var newB = [];
+
+  object_b.value.forEach(item => {
+    newB.push(item);
+  });
+
+  var newArr = collection_a.map(item => {
+    for (var i = 0; i < newB.length; i++) {
+      if (item.key == newB[i]) {
+        item.count --;
+        return item;
+      }
+    }
+    return item;
+  })
+  return newArr;
 }
 
 module.exports = create_updated_collection;
