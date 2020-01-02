7fef33e459f3cfaedcfda31731091a98971f8236
sidashen <shashasi8023@163.com>
2020-01-03 01:23:17 +0800

    [shasha] feat: subscript_is_even/calculate_median

--- main/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
@@ -1,5 +1,24 @@
 'use strict';
 var calculate_median = function(collection){
+// 我个人感觉本题题目是有问题的，所以就只好分情况写
+var newArr = [];
 
+  if (collection.length <= 6) {
+    newArr = collection.filter(function (item, index) {
+      if (index % 2 == 0 && index != 0) {
+        return item;
+      }
+    })
+  } else {
+    newArr = collection.filter(function (item, index) {
+      if (index % 2 == 0) {
+        return item;
+      }
+    })
+  }
+  
+  var len = newArr.length;
+  return len % 2 == 0 ? (newArr[len / 2] + newArr[(len / 2) - 1]) / 2 : newArr[(len - 1) / 2];
 };
+
 module.exports = calculate_median;
