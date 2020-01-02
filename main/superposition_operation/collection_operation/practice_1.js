a62ba872d755a900552ca2d69bf5918d189dd7d9
sidashen <shashasi8023@163.com>
2020-01-02 23:46:09 +0800

    [shasha] feat: collection_operation/practice_1_spec

--- main/superposition_operation/collection_operation/practice_1.js
+++ main/superposition_operation/collection_operation/practice_1.js
@@ -1,8 +1,13 @@
 'use strict';
 
 function hybrid_operation(collection) {
-
   //在这里写入代码
+  var newArr = collection.map(item => item * 3 + 2);
+  var sum = 0;
+  newArr.forEach(item => {
+    sum += item;
+  });
+  return sum;
 }
 
 module.exports = hybrid_operation;
