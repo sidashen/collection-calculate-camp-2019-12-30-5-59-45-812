cdd65f36358b7e59e64cd4f30b4971912a4ecb93
sidashen <shashasi8023@163.com>
2020-01-03 01:03:39 +0800

    [shasha] feat: subscript_is_even/calculate_average

--- main/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
@@ -1,5 +1,17 @@
 'use strict';
 var calculate_average = function(collection){
+  var newArr = collection.filter(function (item, index) {
+    if (index % 2 == 0 && index !== 0) {
+      return item;
+    }
+  });
 
+  var len = newArr.length;
+  var sum = 0;
+  newArr.forEach(item => {
+    sum += item;
+  })
+  return sum / len;
 };
+
 module.exports = calculate_average;
