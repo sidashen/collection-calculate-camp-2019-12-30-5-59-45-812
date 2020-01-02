d7448ea5974b0e43e830fe7f3eff634d1151dee7
sidashen <shashasi8023@163.com>
2020-01-02 18:35:58 +0800

    [shasha] feat: section_1_practice_4

--- main/section_1/practice_4/practice.js
+++ main/section_1/practice_4/practice.js
@@ -1,5 +1,20 @@
 function collect_same_elements(collection_a, object_b) {
   //在这里写入代码
+  var newA = [];
+  var newB = [];
+
+  collection_a.forEach(item => {
+    newA.push(item.key);
+  });
+
+  object_b.value.forEach(item => {
+    newB.push(item);
+  });
+
+  var newArr = newA.filter(function (item) {
+    return newB.indexOf(item) !== -1;
+  });
+  return newArr;
 }
 
 module.exports = collect_same_elements;
