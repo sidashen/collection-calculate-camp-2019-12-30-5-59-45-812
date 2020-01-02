281945eaf1a47d36e3b0dae29218ba67343b5e7a
sidashen <shashasi8023@163.com>
2020-01-02 21:37:14 +0800

    [shasha] feat: section_2_practice_1

--- main/section_2/practice_1/practice.js
+++ main/section_2/practice_1/practice.js
@@ -1,5 +1,24 @@
 function count_same_elements(collection) {
   //在这里写入代码
+  let result = [];
+  for (let item of collection) {
+    let obj = finds(result,item);
+      if (obj) {
+        obj.count ++;
+      } else {
+          result.push({key: item, count: 1});
+        }
+  }
+  return result;
+}
+
+function finds(result, element) {
+  for (let item of result) {
+    if (item.key === element) {
+      return item;
+    }
+  }
+  return null;
 }
 
 module.exports = count_same_elements;
