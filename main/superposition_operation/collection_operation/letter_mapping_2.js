a48e583c827115372bd13b88cd39ee0a26efc6bf
sidashen <shashasi8023@163.com>
2020-01-02 23:43:09 +0800

    [shasha] feat: letter_mapping_2

--- main/superposition_operation/collection_operation/letter_mapping_2.js
+++ main/superposition_operation/collection_operation/letter_mapping_2.js
@@ -1,8 +1,14 @@
 'use strict';
 
 function average_to_letter(collection) {
-
   //在这里写入代码
+  var sum = 0;
+  var count = collection.length;
+  collection.forEach(item => {
+    sum += item;
+  })
+  var average = Math.ceil(sum / count); 
+  return String.fromCharCode(96 + average);
 }
 
 module.exports = average_to_letter;
