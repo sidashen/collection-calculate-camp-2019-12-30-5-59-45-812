7d83ab253327b5acd144de0e393b9343f5aee1f6
sidashen <shashasi8023@163.com>
2020-01-02 23:54:13 +0800

    [shasha] feat: collection_operation/practice_2_spec

--- main/superposition_operation/collection_operation/practice_2.js
+++ main/superposition_operation/collection_operation/practice_2.js
@@ -1,8 +1,12 @@
 'use strict';
 
 function hybrid_operation_to_uneven(collection) {
-
   //在这里写入代码
+  var oddUnion = collection.filter(item => {
+    return item % 2 !== 0;
+  });
+  var newArr = oddUnion.map(item => item * 3 + 2);
+  return newArr;
 }
 
 module.exports = hybrid_operation_to_uneven;
