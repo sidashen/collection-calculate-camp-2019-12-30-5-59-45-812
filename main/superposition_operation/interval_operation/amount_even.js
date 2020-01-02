488452816e53ddb71d9408872dc33fcf8809fe36
sidashen <shashasi8023@163.com>
2020-01-03 00:16:18 +0800

    [shasha] feat: interval_operation/amount_even

--- main/superposition_operation/interval_operation/amount_even.js
+++ main/superposition_operation/interval_operation/amount_even.js
@@ -1,8 +1,15 @@
 'use strict';
 
 function amount_even(collection) {
-
   //在这里写入代码
+  var sum = 0;
+  var collection = [1,2,3,4,5,6,7,8,9,10];
+  collection.forEach(item => {
+    if (item % 2 == 0) {
+      sum += item;
+    }
+  });
+  return sum;
 }
 
 module.exports = amount_even;
--- main/superposition_operation/interval_operation/average_uneven.js
+++ main/superposition_operation/interval_operation/average_uneven.js
@@ -1,8 +1,18 @@
 'use strict';
 
 function average_uneven(collection) {
-
   //在这里写入代码
+  var collection = [1,2,3,4,5,6,7,8,9,10];
+  var newArr = collection.filter(item => {
+    return item % 2 !== 0;
+  });
+
+  var sum = 0;
+  var len = newArr.length;
+  newArr.forEach(item => {
+    sum += item;
+  });
+  return sum / len;
 }
 
 module.exports = average_uneven;
