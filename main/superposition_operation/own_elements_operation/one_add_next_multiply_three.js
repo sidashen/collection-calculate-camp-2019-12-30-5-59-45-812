d4a2b9595837b20cc8acd76258d9b5dde6ea21d1
sidashen <shashasi8023@163.com>
2020-01-03 13:34:27 +0800

    [shasha] feat: one_add_next_multiply_three

--- main/superposition_operation/own_elements_operation/one_add_next_multiply_three.js
+++ main/superposition_operation/own_elements_operation/one_add_next_multiply_three.js
@@ -1,5 +1,12 @@
 'use strict';
-function one_add_next_multiply_three(collection){
-  return [12,24,36,48,60,72,84,96,108,120];
+function one_add_next_multiply_three(collection) {
+  var newArr = [];
+  var newItem = 0;
+  for (let i = 0; i < collection.length - 1; i++) {
+    newItem = collection[i] + collection[i + 1];
+    newArr.push(newItem);
+  }
+  return  newArr.map(item => item * 3);
 }
+
 module.exports = one_add_next_multiply_three;
