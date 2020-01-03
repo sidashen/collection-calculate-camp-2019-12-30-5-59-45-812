8f777fd602140156ab2c111885b1115d6c9054ed
sidashen <shashasi8023@163.com>
2020-01-03 13:29:25 +0800

    [shasha] feat: even_asc_odd_desc

--- main/superposition_operation/own_elements_operation/even_asc_odd_desc.js
+++ main/superposition_operation/own_elements_operation/even_asc_odd_desc.js
@@ -1,5 +1,14 @@
 'use strict';
 var even_asc_odd_desc = function(collection){
-
+  const oddArr = collection.filter(item => item % 2 !== 0);
+  const evenArr = collection.filter(item => item % 2 == 0);
+  const newOddArr = oddArr.sort(function (value1, value2) {
+    return value2 - value1;
+  });
+  const newEvenArr = evenArr.sort(function (value1, value2) {
+    return value1 - value2;
+  });
+  return newEvenArr.concat(newOddArr);
 };
+
 module.exports = even_asc_odd_desc;
