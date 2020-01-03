4ba0945f10642ac18fcd9e0131f98830601a5463
sidashen <shashasi8023@163.com>
2020-01-03 18:29:05 +0800

    [shasha] feat: compute_chain

--- main/reduce/compute_chain_median.js
+++ main/reduce/compute_chain_median.js
@@ -1,15 +1,16 @@
 'use strict';
 
-function ListNode(val){
-  this.val = val;
-  this.next = null;
-}
-
 function compute_chain_median(collection) {
   //在这里写入代码
-  
+  var newArr = collection.split('->').map(item => Number(item));
+  var len = newArr.length;
+  var medianNum = 0;
+  if (len % 2 == 0) {
+    medianNum = (newArr[len / 2] + newArr[(len / 2) - 1] - 2) / 2;
+  } else {
+    medianNum = newArr[(len - 1) / 2];
+  }
+  return medianNum;
 }
-
-
 
 module.exports = compute_chain_median;
--- main/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
+++ main/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
@@ -1,5 +1,9 @@
 'use strict';
 function rank_by_two_large_one_small(collection){
   //这里写代码。。。
+  var orderArr = collection.sort(function (value1, value2) {
+    return value1 - value2;
+  });
+  
 }
 module.exports = rank_by_two_large_one_small;
