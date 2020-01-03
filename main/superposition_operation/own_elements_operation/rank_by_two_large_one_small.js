3e25923bc81e4fb89c64a8a2c333b280b56fbc8f
sidashen <shashasi8023@163.com>
2020-01-03 19:00:34 +0800

    [shasha] feat: rank_by_two_large_one_small

--- main/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
+++ main/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
@@ -1,9 +1,18 @@
 'use strict';
-function rank_by_two_large_one_small(collection){
+function rank_by_two_large_one_small(collection) {
   //这里写代码。。。
   var orderArr = collection.sort(function (value1, value2) {
     return value1 - value2;
   });
-  
+  var newArr = orderArr.filter(function (item, index) {
+    if (index % 3 !== 0 || index == 6) {
+      return item;
+    }
+  });
+
+  newArr.splice(2, 0, orderArr[0]);
+  newArr.splice(5, 0, orderArr[3]);
+  return newArr;
 }
+
 module.exports = rank_by_two_large_one_small;
